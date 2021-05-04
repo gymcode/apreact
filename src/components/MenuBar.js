5722f1c88696fd70353b7ebb11295d2b77699958
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-05-04 22:01:08 +0000

    ode to sleep

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,2 +1,25 @@
 import React from 'react'; 
-import {} from 'semantic-ui-react'; 
+import {Menu, Segment} from 'semantic-ui-react';  
+
+const MenuBar = ()=>{
+
+    const [activeItem, setActiveItem] = React.useState()
+
+    return (
+        <div>
+            <Menu pointing secondary>
+                <Menu.Item name='home' 
+                    active={setActiveItem('home')}
+                    onClick={handleClick}
+                    />
+                <Menu.Item name='home' 
+                    active={setActiveItem('home')}
+                    onClick={handleClick}
+                    />
+                <Menu.Menu position="right">
+                    <Menu.Item/>
+                </Menu.Menu>
+            </Menu>
+        </div>
+    )
+}
