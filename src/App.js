93e13a061ba4e15bfe2a19b0684103345ff8b8f0
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-05-04 23:05:56 +0000

    sleep

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import 'semantic-ui-css/semantic'
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import 'semantic-ui-css/semantic'
 
 // importing component
 const LoginComponent = React.lazy(()=> import('./pages/Authentication/Login'))
 const RegisterComponent = React.lazy(()=> import('./pages/Authentication/Registration'))
-
+const HomeComponent = React.lazy(()=> import('./pages/Home'))
 
 function App() {
   return (
     <Router>
-      {/* <Route path={"/"} exact component={HomeComponent} /> */}
+      <Route path={"/"} exact component={HomeComponent} />
       <Route path={"/login"} exact component={LoginComponent}/>
       <Route path={"/register"} exact component={RegisterComponent}/>
     </Router>
