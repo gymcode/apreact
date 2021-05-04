3f317b6762f749c7ded92c9719530cd8ca4cc58b
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-05-04 22:21:00 +0000

    added the home component"

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -3,23 +3,35 @@ import {Menu, Segment} from 'semantic-ui-react';
 
 const MenuBar = ()=>{
 
-    const [activeItem, setActiveItem] = React.useState()
+    const [activeItem, setActiveItem] = React.useState('')
+
+    function handleClick(e, {name}){ 
+
+    }
 
     return (
         <div>
             <Menu pointing secondary>
-                <Menu.Item name='home' 
-                    active={setActiveItem('home')}
-                    onClick={handleClick}
-                    />
-                <Menu.Item name='home' 
+                <Menu.Item 
+                    name='home' 
                     active={setActiveItem('home')}
                     onClick={handleClick}
                     />
                 <Menu.Menu position="right">
-                    <Menu.Item/>
+                    <Menu.Item 
+                        name='login' 
+                        active={setActiveItem('login')}
+                        onClick={handleClick}
+                        />
+                    <Menu.Item 
+                        name='register' 
+                        active={setActiveItem('register')}
+                        onClick={handleClick}
+                         />
                 </Menu.Menu>
             </Menu>
         </div>
     )
 }
+
+export default MenuBar
---
+++ src/pages/Home/main.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+const MainComponent = () =>{
+    return(
+        <div>
+            this is homeg
+        </div>
+    )
+}
+
+export default MainComponent
