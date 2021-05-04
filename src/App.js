313c5ec00da0347d6f77d5fad1e2d74a827c128e
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-05-04 21:47:10 +0000

    creating a menu bar component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 // importing component
-import 
+const LoginComponent = React.lazy(()=> import('./pages/Authentication/Login'))
+const RegisterComponent = React.lazy(()=> import('./pages/Authentication/Registration'))
 
 
 function App() {
   return (
     <Router>
-      <Route path={"/"} exact component={HomeComponent} />
+      {/* <Route path={"/"} exact component={HomeComponent} /> */}
       <Route path={"/login"} exact component={LoginComponent}/>
       <Route path={"/register"} exact component={RegisterComponent}/>
     </Router>
