b4086b0a9e5e3892e704a87e8e377c7acf035d48
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-05-29 23:56:15 +0000

    lost

--- src/pages/Home/main.js
+++ src/pages/Home/main.js
@@ -1,4 +1,4 @@
-import {React} from 'react'; 
+import React from 'react'; 
 import {Grid, Image} from 'semantic-ui-react'
 import {gql, useQuery} from "@apollo/client"
 
@@ -24,22 +24,27 @@ const GetPost = gql
 
 const MainComponent = () =>{
 
-    const {loading, data, error} = useQuery(GetPost)
+    const {loading, data: {getPosts}, error} = useQuery(GetPost)
 
-    console.log(data)
+    console.log(getPosts)
     return(
         <div style={{paddingTop: 30}}>
             <Grid columns={3} divided>
                 <Grid.Row>
                     <Grid.Column>
-                        <div>dads</div>
+                        <div>Recent Posts</div>
                     </Grid.Column>
-                    <Grid.Column>
-                        <div>dads</div>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <div>dads</div>
-                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                  {loading ? (
+                      <h1>Loading posts ... </h1>
+                  ) : (
+                     getPosts && getPosts.map((items)=> (
+                        <Grid.Column>
+                            <h2>{items.body}</h2>
+                        </Grid.Column>
+                     ))
+                  )}
                 </Grid.Row>
             </Grid>
         </div>
